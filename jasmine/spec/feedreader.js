eff1d28d89cffd33c6bf41a3acb394dda36fe60c
Rashad Attaf <rashadattaf@gmail.com>
2019-05-12 22:00:29 +0300

    feat : new test to ensures that feed URL's are defined and not empty.
    a test that loops through each feed in the allFeeds object
    and ensures it has a URL defined
    and that the URL is not empty.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('URL\'s are defined and not empty', function () {
+            for (const feed of allFeeds) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
