c21e4ce863d8e6867b4ccfeb382383323aa19f8e
Rashad Attaf <rashadattaf@gmail.com>
2019-05-12 22:40:17 +0300

    feat : Write a new test suite named "New Feed Selection".

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -118,9 +118,11 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
+    describe('New Feed Selection', function () {
+         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
+
 }());
